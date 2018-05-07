2cba47c0fb1570dc34d17feba3c1d151b178ef87
Naumi <naumi.varma1@gmail.com>
2018-05-07 11:04:18 -0400

    I made a Cell class

---
+++ src/utils/Cell.java
@@ -0,0 +1,36 @@
+package utils;
+
+public class Cell<anyType> {
+    private anyType value;	//the actual Object that the Cell stores <can be any type>
+    private int row;		//the row value it is simulated to be stored at
+    private int col;		//the column value it is simulated to be stored at
+    private int key;
+
+    public Cell(int r,int c, anyType v)
+    {
+        value = v;
+        row = r;
+        col = c;
+
+    }
+
+    public void setValue(anyType v)
+    {
+        value = v;
+    }
+
+    public int getRow()
+    {
+        return row;
+    }
+
+    public int getCol()
+    {
+        return col;
+    }
+
+    public anyType getValue()
+    {
+        return value;
+    }
+}
