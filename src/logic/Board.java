27eb2a6f93b89e4b6db4b01045bf4793af87b0f9
Naumi <naumi.varma1@gmail.com>
2018-05-21 11:11:28 -0400

    I added comments for modifying the Pill class

--- src/logic/Board.java
+++ src/logic/Board.java
@@ -1,8 +1,48 @@
 package logic;
 
+import utils.SparseMatrix;
+
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Board {
 
+    public static void main(String args[]) {
+
+        SparseMatrix<Pill> board = new SparseMatrix<>(16, 8);
+
+        boolean playing = true;
+
+        while (playing) {
+            int randColor = (int) (Math.random() * 3);
+            int randOrient = (int) (Math.random() * 4);
+
+            //if()
+            Pill curr = new Pill(Color.RED, Pill.Orientation.LEFT);
+
+
+        }
+    }
+
+    public void moveLeft(SparseMatrix grid, Pill current) {
+        if (grid[current.getRow()][current.getCol()- 1 ].isEmpty) {
+            grid.add(current.getRow(), current.getCol() - 1);
+            grid.remove(current.getRow(),current.getCol());
+        }
+    }
+
+    public void moveRight(SparseMatrix grid, Pill current) {
+        if (grid[current.getRow()][current.getCol()+ 1 ].isEmpty) {
+            grid.add(current.getRow(), current.getCol() + 1);
+            grid.remove(current.getRow(),current.getCol());
+        }
+    }
+
+    public void moveDown(SparseMatrix grid, Pill current) {
+        if (grid[current.getRow() + 1][current.getCol()].isEmpty) {
+            grid.add(current.getRow() + 1, current.getCol() - 1);
+            grid.remove(current.getRow(),current.getCol());
+        }
+    }
 
 }
