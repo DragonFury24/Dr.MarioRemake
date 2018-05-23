3c01886ca2fcafedb8919a26116730d7963a62f8
Naumi <naumi.varma1@gmail.com>
2018-05-23 10:29:23 -0400

    I added the constructor and methods for the Queue class.

--- src/utils/Queue.java
+++ src/utils/Queue.java
@@ -1,5 +1,34 @@
 package utils;
 
 public class Queue<E> {
+    private java.util.LinkedList<E> list;
+
+    public Queue() {
+        list = new java.util.LinkedList<E>();
+    }
+
+    public void add(E x) {
+        list.add(x);
+    }
+
+    public E remove() {
+        return list.removeFirst();
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+
+    public E peek() {
+        return list.peek();
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public String toString() {
+        return list.toString();
+    }
 
 }
